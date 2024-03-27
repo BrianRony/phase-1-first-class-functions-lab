c53493442b91c0c696407bbab176ededd119089b
Brian Ngetich <kipronobrian97@gmail.com>
2024-03-28 01:14:18 +0300

    Functions added

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution in this file!
+const drivers = ["Antonia","Nuru","Amari","Mo"];
+
+function returnFirstTwoDrivers (drivers){
+    return (drivers.slice(0,2));
+}
+function returnLastTwoDrivers (drivers){
+    return (drivers.slice(2));
+}
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(fare){
+    return function(){
+        return fare * fare;
+    }
+}
+const fareDoubler = (fare) => fare *2;
+const fareTripler = (fare) => fare *3;
+
+function selectDifferentDrivers (drivers, returnDrivers){
+    return (returnDrivers(drivers));
+}
+console.log (selectDifferentDrivers(returnFirstTwoDrivers));
